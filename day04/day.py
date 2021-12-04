7f543807d26be84817d599b270d061575b821890
Rey Abolofia <rey.abolofia@stitchfix.com>
2021-12-03 21:24:17 -0800

    Problems from day 4.

---
+++ day04/day.py
@@ -0,0 +1,45 @@
+import numpy as np
+
+def part1(inputs):
+    nums, boards = inputs
+    for num in nums:
+        boards[boards==num] = 0
+        for board in boards:
+            if (board.sum(axis=0) == 0).any() or \
+                    (board.sum(axis=1) == 0).any():
+                return board.sum() * num
+
+def part2(inputs):
+    nums, boards = inputs
+    new_boards = []
+    for num in nums:
+        boards[boards==num] = 0
+        for board in boards:
+            if (board.sum(axis=0) == 0).any() or \
+                    (board.sum(axis=1) == 0).any():
+                pass
+            else:
+                new_boards.append(board)
+        if not len(new_boards):
+            return board.sum() * num
+        boards, new_boards = np.array(new_boards), []
+
+def read_inputs():
+    with open('input.txt') as f:
+        return f.read().strip()
+
+def process(raw):
+    numstr, boardstr = raw.split('\n', maxsplit=1)
+    nums = [int(n) for n in numstr.split(',')]
+    boards = []
+    for b in boardstr.split('\n\n'):
+        boards.append([
+            [int(d) for d in l.split()]
+            for l in b.split('\n') if l
+        ])
+    return nums, np.array(boards)
+
+if __name__ == '__main__':
+    inputs = process(read_inputs())
+    print(part1(inputs))
+    print(part2(inputs))
---
+++ day04/day_test.py
@@ -0,0 +1,34 @@
+import pytest
+from day import part1, part2, process
+
+_test_input = """
+7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,8,19,3,26,1
+
+22 13 17 11  0
+ 8  2 23  4 24
+21  9 14 16  7
+ 6 10  3 18  5
+ 1 12 20 15 19
+
+ 3 15  0  2 22
+ 9 18 13 17  5
+19  8  7 25 23
+20 11 10 24  4
+14 21 16 12  6
+
+14 21 17 24  4
+10 16 15  9 19
+18  8 23 26 20
+22 11 13  6  5
+ 2  0 12  3  7
+""".strip()
+_test_part1_expect = 4512
+_test_part2_expect = 1924
+
+def test_part1():
+    inputs = process(_test_input)
+    assert _test_part1_expect == part1(inputs)
+
+def test_part2():
+    inputs = process(_test_input)
+    assert _test_part2_expect == part2(inputs)
