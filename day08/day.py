0164af704bd05b47c9d830d760bacf40683c2895
Rey Abolofia <rey.abolofia@stitchfix.com>
2021-12-07 22:58:34 -0800

    Code cleanup.

--- day08/day.py
+++ day08/day.py
@@ -33,48 +33,48 @@ def part2(puzzle_input):
                 for i in ins:
                     given_to_expected[d][i] = 0
 
+    def translate(digit):
+        num = 0
+        for d in digit:
+            for i, j in enumerate(given_to_expected[d]):
+                num += j << i
+        return _translate[num]
+
+    def assign_digit(digit):
+        length = len(digit)
+        if length == 2:
+            assign(digit, (0, 1, 3, 4, 6), (2, 5))
+        elif length == 3:
+            assign(digit, (1, 3, 4, 6), (0, 2, 5))
+        elif length == 4:
+            assign(digit, (0, 4, 6), (1, 2, 3, 5))
+        elif length == 5:
+            assign(digit, [], (0, 3, 6))
+        elif length == 6:
+            assign(digit, [], (0, 1, 5, 6))
+
+    def clean_uniques():
+        for key, values in given_to_expected.items():
+            if sum(values) > 1:
+                continue
+            d = values.index(1)
+            for other_key, other_values in given_to_expected.items():
+                if key == other_key:
+                    continue
+                other_values[d] = 0
+
+    def digits_to_assign():
+        return any(sum(v) > 1 for v in given_to_expected.values())
+
     total = 0
     for inputs, outputs in puzzle_input:
         given_to_expected = {'a': [1]*7, 'b': [1]*7, 'c': [1]*7, 'd': [1]*7,
                 'e': [1]*7, 'f': [1]*7, 'g': [1]*7}
-        while sum(sum(v) for v in given_to_expected.values()) > 7:
+
+        while digits_to_assign():
             for digit in inputs + outputs:
-                length = len(digit)
-
-                if length == 2:
-                    # one
-                    assign(digit, (0, 1, 3, 4, 6), (2, 5))
-                elif length == 3:
-                    # seven
-                    assign(digit, (1, 3, 4, 6), (0, 2, 5))
-                elif length == 4:
-                    # four
-                    assign(digit, (0, 4, 6), (1, 2, 3, 5))
-                elif length == 5:
-                    # two, three, five
-                    assign(digit, [], (0, 3, 6))
-                elif length == 6:
-                    # zero, six, nine
-                    assign(digit, [], (0, 1, 5, 6))
-                elif length == 7:
-                    # eight
-                    pass
-
-            for key, values in given_to_expected.items():
-                if sum(values) > 1:
-                    continue
-                d = [i for i, j in enumerate(values) if j][0]
-                for other_key, other_values in given_to_expected.items():
-                    if key == other_key:
-                        continue
-                    other_values[d] = 0
-
-        def translate(digit):
-            num = 0
-            for d in digit:
-                for i, j in enumerate(given_to_expected[d]):
-                    num += j << i
-            return _translate[num]
+                assign_digit(digit)
+            clean_uniques()
 
         num = 0
         for digit in outputs:
