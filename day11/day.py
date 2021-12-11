d099f8b7f84433742ebce1cf8690337bc110b99d
Rey Abolofia <rey.abolofia@stitchfix.com>
2021-12-10 22:39:32 -0800

    Now cleaned.

--- day11/day.py
+++ day11/day.py
@@ -33,102 +33,22 @@ def part1(matrix):
             matrix[k,l] += 1
 
     flashes = 0
-    print('----------------------------------------')
-    print('before step: ', 1)
-    print(matrix)
     for step in range(100):
-        print('----------------------------------------')
         matrix += 1
         octos = {}
         while True:
             flashed = 0
             now = matrix > 9
-            #print()
-            #print(matrix)
             for i in range(height):
                 for j in range(width):
                     if now[i,j] and (i, j) not in octos:
                         increase_neighbors(matrix, i, j)
                         flashed += 1
-                        flashes += 1
                         octos[(i, j)] = True
+            flashes += flashed
             if not flashed:
                 break
         matrix[matrix>9] = 0
-        print('after step: ', step+1)
-        print(matrix)
-
-
-        if step == 0:  # step 1
-            expect = """
-            6594254334
-            3856965822
-            6375667284
-            7252447257
-            7468496589
-            5278635756
-            3287952832
-            7993992245
-            5957959665
-            6394862637
-            """.replace('\n', '').replace(' ', '').strip()
-            actual = ''.join([''.join(str(i) for i in m) for m in matrix])
-            #assert expect == actual
-        if step == 1:  # step 2
-            expect = """
-            8807476555
-            5089087054
-            8597889608
-            8485769600
-            8700908800
-            6600088989
-            6800005943
-            0000007456
-            9000000876
-            8700006848
-            """.replace('\n', '').replace(' ', '').strip()
-            actual = ''.join([''.join(str(i) for i in m) for m in matrix])
-            #assert expect == actual
-        if step == 2:  # step 3
-            expect = """
-            0050900866
-            8500800575
-            9900000039
-            9700000041
-            9935080063
-            7712300000
-            7911250009
-            2211130000
-            0421125000
-            0021119000
-            """.replace('\n', '').replace(' ', '').strip()
-            actual = ''.join([''.join(str(i) for i in m) for m in matrix])
-
-            RED = '\033[0;31m'
-            COLOR_OFF = '\033[0m'
-            print('actual:')
-            for i in range(height):
-                for j in range(width):
-                    if actual[i*width+j] != expect[i*width+j]:
-                        print(RED, end='')
-                    else:
-                        print(COLOR_OFF, end='')
-                    print(actual[i*width+j], end='')
-                print()
-            print(COLOR_OFF)
-            print('expect:')
-            for i in range(height):
-                for j in range(width):
-                    if actual[i*width+j] != expect[i*width+j]:
-                        print(RED, end='')
-                    else:
-                        print(COLOR_OFF, end='')
-                    print(expect[i*width+j], end='')
-                print()
-            print(COLOR_OFF)
-
-            #assert expect == actual
-
 
     return flashes
 
