a5ee327ee1d001347a11dee766af2ba6762910b0
Rey Abolofia <rey.abolofia@stitchfix.com>
2021-12-14 14:26:56 -0800

    Complete part 2.

--- day13/day.py
+++ day13/day.py
@@ -21,11 +21,37 @@ def part1(inputs):
     return (matrix > 0).sum()
 
 def part2(inputs):
-    pass
+
+    matrix, folds = inputs
+    for axis, val in folds:
+        a, _, b = np.split(matrix, [val, val + 1], axis=axis)
+        b = np.flip(b, axis=axis)
+
+        ashape, bshape = a.shape[axis], b.shape[axis]
+        base = a if ashape >= bshape else b
+        add = b if ashape >= bshape else a
+
+        ashape, bshape = add.shape[axis], base.shape[axis]
+        slc = slice(bshape - ashape, bshape)
+        if axis == 0:
+            base[slc] += add
+        else:
+            base[:,slc] += add
+        matrix = base
+
+    return code(matrix)
 
 def read_inputs():
     with open('input.txt') as f:
         return f.read().strip()
+
+def code(matrix):
+    code = ''
+    for m in matrix:
+        for d in m:
+            code += '#' if d else ' '
+        code += '\n'
+    return code
 
 def process(raw):
     dots, folds = [], []
@@ -42,6 +68,15 @@ def process(raw):
     matrix = np.zeros((dots.max() + 1, dots.max() + 1))
     for x, y in dots:
         matrix[y,x] = 1
+
+    # remove extra rows
+    while matrix[-1].sum() == 0:
+        matrix = matrix[:-1]
+
+    # remove extra columns
+    while matrix[:,-1].sum() == 0:
+        matrix = matrix[:,:-1]
+
     return matrix, folds
 
 if __name__ == '__main__':
