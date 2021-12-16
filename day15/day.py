466863ece7006e33977f743ae90877584980fd8b
Rey Abolofia <rey.abolofia@stitchfix.com>
2021-12-15 16:39:00 -0800

    Slow solution for part 2.

--- day15/day.py
+++ day15/day.py
@@ -1,3 +1,6 @@
+import sys
+sys.setrecursionlimit(10**5)
+
 def memoize(fn):
     _cache = {}
     def wrap(*args):
@@ -45,15 +48,63 @@ def part1(cave):
     global_lowest = [9 * height * width]
     return search(0, 0, 0)
 
-def part2(inputs):
-    pass
+def part2(cave):
+
+    height = len(cave)
+    width = len(cave[0])
+
+    neighbors_of = []
+    for i in range(height):
+        row = []
+        for j in range(width):
+            vals = []
+            if j < width - 1:
+                vals.append((i, j+1))
+            if i < height - 1:
+                vals.append((i+1, j))
+            if j > 0:
+                vals.append((i, j-1))
+            if i > 0:
+                vals.append((i-1, j))
+            row.append(vals)
+        neighbors_of.append(row)
+
+    @memoize
+    def search(i, j, total):
+        if total >= global_lowest[0]:
+            return total
+        if i == height - 1 and j == width - 1:
+            if total < global_lowest[0]:
+                global_lowest[0] = total
+            return total
+        choices = []
+        for k, l in neighbors_of[i][j]:
+            choices.append(search(k, l, total + cave[k][l]))
+        return min(choices)
+
+    global_lowest = [9 * height * width]
+    return search(0, 0, 0)
 
 def read_inputs():
     with open('input.txt') as f:
         return f.read().strip()
 
-def process(raw):
+def process_part1(raw):
     return [[int(i) for i in row] for row in raw.split()]
+
+def process_part2(raw):
+    cave = []
+    for row_repeat in range(5):
+        for row in raw.split():
+            new_row = []
+            for col_repeat in range(5):
+                for risk in row:
+                    new_val = int(risk) + row_repeat + col_repeat
+                    if new_val > 9:
+                        new_val -= 9
+                    new_row.append(new_val)
+            cave.append(new_row)
+    return cave
 
 if __name__ == '__main__':
     inputs = process(read_inputs())
--- day15/day_test.py
+++ day15/day_test.py
@@ -1,5 +1,5 @@
 import pytest
-from day import part1, part2, process
+from day import part1, part2, process_part1, process_part2
 
 _test_input = """
 1163751742
@@ -14,12 +14,12 @@ _test_input = """
 2311944581
 """.strip()
 _test_part1_expect = 40
-_test_part2_expect = None
+_test_part2_expect = 315
 
 def test_part1():
-    inputs = process(_test_input)
+    inputs = process_part1(_test_input)
     assert _test_part1_expect == part1(inputs)
 
 def test_part2():
-    inputs = process(_test_input)
+    inputs = process_part2(_test_input)
     assert _test_part2_expect == part2(inputs)
