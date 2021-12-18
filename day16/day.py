ad1d54e9c75287838918b995671a5b01e9ad565e
Rey Abolofia <rey.abolofia@stitchfix.com>
2021-12-17 23:19:07 -0800

    Part2 type_id 4.

--- day16/day.py
+++ day16/day.py
@@ -14,12 +14,16 @@ def parse_message(bits, bitmod):
     bitmod >>= 3
     type_id, bits = divmod(bits, bitmod)
 
+    num = 0
     if type_id == 4:
         while True:
             bitmod >>= 5
-            num, bits = divmod(bits, bitmod)
-            if not num // 16:
-                return versions, bits, bitmod
+            numbits, bits = divmod(bits, bitmod)
+            num <<= 4
+            key, numbits = divmod(numbits, 16)
+            num += numbits
+            if not key:
+                return versions, bits, bitmod, num
 
     bitmod >>= 1
     length_type_id, bits = divmod(bits, bitmod)
@@ -28,7 +32,7 @@ def parse_message(bits, bitmod):
         bitmod >>= 11
         subpacket_count, bits = divmod(bits, bitmod)
         for _ in range(subpacket_count):
-            version, bits, bitmod = parse_message(bits, bitmod)
+            version, bits, bitmod, _ = parse_message(bits, bitmod)
             versions += version
     else:
         bitmod >>= 15
@@ -37,13 +41,19 @@ def parse_message(bits, bitmod):
         subbits, bits = divmod(bits, bitmod)
         subbitmod = 1 << subpacket_length
         while subbits:
-            version, subbits, subbitmod = parse_message(subbits, subbitmod)
+            version, subbits, subbitmod, _ = parse_message(subbits, subbitmod)
             versions += version
 
-    return versions, bits, bitmod
+    return versions, bits, bitmod, num
 
-def part2(inputs):
-    pass
+def part2(msg):
+    bits, bitmod, versions = 0, 1, 0
+    for d in msg:
+        i = int(d, 16)
+        bits <<= 4
+        bits += i
+        bitmod <<= 4
+    return parse_message(bits, bitmod)[-1]
 
 def read_inputs():
     with open('input.txt') as f:
--- day16/day_test.py
+++ day16/day_test.py
@@ -15,3 +15,17 @@ _test_part1 = (
 def test_part1(raw, expect):
     inputs = process(raw)
     assert expect == part1(inputs)
+
+_test_part2 = (
+        ('D2FE28', 2021),
+        #('38006F45291200', 9),
+        #('EE00D40C823060', 14),
+        #('8A004A801A8002F478', 16),
+        #('620080001611562C8802118E34', 12),
+        #('C0015000016115A2E0802F182340', 23),
+        #('A0016C880162017C3686B18A3D4780', 31),
+)
+
+@pytest.mark.parametrize('n,expect', _test_part2)
+def test_part2(n, expect):
+    assert expect == part2(n)
