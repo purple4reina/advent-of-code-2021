a22a48888a7847cf0da92c4f505d7fde1d1b4d85
Rey Abolofia <rey.abolofia@stitchfix.com>
2021-12-17 23:27:05 -0800

    Implement all type_id values.

--- day16/day.py
+++ day16/day.py
@@ -14,8 +14,8 @@ def parse_message(bits, bitmod):
     bitmod >>= 3
     type_id, bits = divmod(bits, bitmod)
 
-    num = 0
     if type_id == 4:
+        num = 0
         while True:
             bitmod >>= 5
             numbits, bits = divmod(bits, bitmod)
@@ -27,13 +27,16 @@ def parse_message(bits, bitmod):
 
     bitmod >>= 1
     length_type_id, bits = divmod(bits, bitmod)
+    nums = []
 
     if length_type_id:
         bitmod >>= 11
         subpacket_count, bits = divmod(bits, bitmod)
         for _ in range(subpacket_count):
-            version, bits, bitmod, _ = parse_message(bits, bitmod)
+            version, bits, bitmod, num = parse_message(bits, bitmod)
             versions += version
+            nums.append(num)
+
     else:
         bitmod >>= 15
         subpacket_length, bits = divmod(bits, bitmod)
@@ -41,8 +44,26 @@ def parse_message(bits, bitmod):
         subbits, bits = divmod(bits, bitmod)
         subbitmod = 1 << subpacket_length
         while subbits:
-            version, subbits, subbitmod, _ = parse_message(subbits, subbitmod)
+            version, subbits, subbitmod, num = parse_message(subbits, subbitmod)
             versions += version
+            nums.append(num)
+
+    if type_id == 0:
+        num = sum(nums)
+    if type_id == 1:
+        num = 1
+        for n in nums:
+            num *= n
+    if type_id == 2:
+        num = min(nums)
+    if type_id == 3:
+        num = max(nums)
+    if type_id == 5:
+        num = int(nums[0] > nums[1])
+    if type_id == 6:
+        num = int(nums[0] < nums[1])
+    if type_id == 7:
+        num = int(nums[0] == nums[1])
 
     return versions, bits, bitmod, num
 
