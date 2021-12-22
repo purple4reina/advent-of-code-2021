659f063c832f02ac92e4df371f350dad28c8ba9c
Rey Abolofia <rey.abolofia@stitchfix.com>
2021-12-22 01:25:57 -0800

    Part 2 tests pass but is very slow.

--- day22/day.py
+++ day22/day.py
@@ -10,8 +10,56 @@ def part1(steps):
         reactor[tuple(section)] = action
     return reactor.sum()
 
-def part2(inputs):
-    pass
+def part2(steps):
+    total, prev_steps, new_prev_steps = 0, [], []
+    for step, (action, ranges) in enumerate(steps):
+        print('step: ', step)
+        (xs, xe), (ys, ye), (zs, ze) = ranges
+        total += (xe - xs + 1) * (ye - ys + 1) * (ze - zs + 1) * action
+        if action:
+            new_prev_steps.append((-1, ranges))
+        for paction, pranges in prev_steps:
+            (pxs, pxe), (pys, pye), (pzs, pze) = pranges
+            val = (min(xe,pxe) - max(xs,pxs) + 1) * \
+                    (min(ye,pye) - max(ys,pys) + 1) * \
+                    (min(ze,pze) - max(zs,pzs) + 1) * paction
+            if val:
+                total += val
+                new_prev_steps.append((-paction,
+                       ((max(xs,pxs), min(xe,pxe)),
+                        (max(ys,pys), min(ye,pye)),
+                        (max(zs,pzs), min(ze,pze)))))
+        prev_steps = new_prev_steps.copy()
+    return total
+
+    """
+    on x=1..3,y=1..3,z=1..3
+    off x=2..4,y=2..4,z=2..4
+    on x=1..5,y=1..5,z=1..5
+
+    on sqr a
+      add all           a           27
+    off sqr b
+      sub inter         -a&b        -8
+    on sqr c
+      add all           c           125
+      sub inter        -a&c         -27
+      add inter-inter  a&b&c        8
+
+          +------------+
+          | a-ON       |
+          |    +-------+------+
+          |  0 |       |      |
+          |    |   0   | b-OFF|
+    +-----+----+--+    |      |
+    |     | 19 |8 |    |      |
+    |     +----+--+----+      |
+    |          |19|     0     |
+    |  c-ON    |  |           |
+    |          +--+-----------+
+    |     79      |
+    +-------------+
+    """
 
 def read_inputs():
     with open('input.txt') as f:
@@ -22,7 +70,7 @@ def process(raw):
     for line in raw.split('\n'):
         step = []
         line = line.strip()
-        step.append(line.startswith('on'))
+        step.append(int(line.startswith('on')))
         regions = []
         for region in line.split()[1].split(','):
             start, stop = map(int, region.split('=')[1].split('..'))
