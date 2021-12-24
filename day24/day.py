cd7d2330fb1568143b236807f874fd8d4cd947f2
Rey Abolofia <rey.abolofia@stitchfix.com>
2021-12-24 02:23:43 -0800

    Write some tests.

--- day24/day.py
+++ day24/day.py
@@ -1,38 +1,40 @@
+glbs = {'w': 0, 'x': 0, 'y': 0, 'z': 0}
+for i in range(-99, 100):
+    glbs[str(i)] = i
+
+def validate(instructions, n):
+    num = []
+    while n:
+        n, r = divmod(n, 10)
+        if r == 0:
+            return False
+        num.append(r)
+    glbs['w'] = glbs['x'] = glbs['y'] = glbs['z'] = 0
+
+    for fn, ab in instructions:
+        if fn == 'inp':
+            glbs[ab] = num.pop(0)
+            continue
+        a, b = ab
+        if fn == 'add':
+            glbs[a] += glbs[b]
+        elif fn == 'mul':
+            glbs[a] *= glbs[b]
+        elif fn == 'eql':
+            glbs[a] = int(glbs[a] == glbs[b])
+        elif fn == 'mod':
+            glbs[a] %= glbs[b]
+        else:
+            glbs[a] //= glbs[b]
+
+    return glbs['z']
+
 def part1(instructions):
-
-    glbs = {'w': 0, 'x': 0, 'y': 0, 'z': 0}
-    for i in range(-99, 100):
-        glbs[str(i)] = i
-
-    def validate(n):
-        num = []
-        while n:
-            n, r = divmod(n, 10)
-            if r == 0:
-                return False
-            num.append(r)
-        glbs['w'] = glbs['x'] = glbs['y'] = glbs['z'] = 0
-
-        for fn, ab in instructions:
-            if fn == 'inp':
-                glbs[ab] = num.pop(0)
-                continue
-            a, b = ab
-            if fn == 'add':
-                glbs[a] += glbs[b]
-            elif fn == 'mul':
-                glbs[a] *= glbs[b]
-            elif fn == 'eql':
-                glbs[a] = int(glbs[a] == glbs[b])
-            elif fn == 'mod':
-                glbs[a] %= glbs[b]
-            else:
-                glbs[a] //= glbs[b]
-
-        return glbs['z'] == 0
-
     for num in range(10**14, 0, -1):
-        if validate(num):
+        val = validate(instructions, num)
+        if val is False:
+            continue
+        if val == 0:
             return num
 
 def part2(inputs):
