e8680b0a84cf7af86c517570f1139af51fd1d07b
Rey Abolofia <rey.abolofia@stitchfix.com>
2021-12-24 19:38:19 -0800

    Directly use the text to make it.

--- day24/day.py
+++ day24/day.py
@@ -1,7 +1,3 @@
-glbs = {'w': 0, 'x': 0, 'y': 0, 'z': 0}
-for i in range(-99, 100):
-    glbs[str(i)] = i
-
 def validate(instructions, n):
     num = []
     while n:
@@ -9,25 +5,263 @@ def validate(instructions, n):
         if r == 0:
             return False
         num.append(r)
-    glbs['w'] = glbs['x'] = glbs['y'] = glbs['z'] = 0
 
-    for fn, ab in instructions:
-        if fn == 'inp':
-            glbs[ab] = num.pop(0)
-            continue
-        a, b = ab
-        if fn == 'add':
-            glbs[a] += glbs[b]
-        elif fn == 'mul':
-            glbs[a] *= glbs[b]
-        elif fn == 'eql':
-            glbs[a] = int(glbs[a] == glbs[b])
-        elif fn == 'mod':
-            glbs[a] %= glbs[b]
-        else:
-            glbs[a] //= glbs[b]
+    w = x = y = z = 0
 
-    return glbs['z']
+    w = num.pop(0)
+    x *= 0
+    x += z
+    x %= 26
+    z //= 1
+    x += 15
+    x = int(x == w)
+    x = int(x == 0)
+    y *= 0
+    y += 25
+    y *= x
+    y += 1
+    z *= y
+    y *= 0
+    y += w
+    y += 4
+    y *= x
+    z += y
+    w = num.pop(0)
+    x *= 0
+    x += z
+    x %= 26
+    z //= 1
+    x += 14
+    x = int(x == w)
+    x = int(x == 0)
+    y *= 0
+    y += 25
+    y *= x
+    y += 1
+    z *= y
+    y *= 0
+    y += w
+    y += 16
+    y *= x
+    z += y
+    w = num.pop(0)
+    x *= 0
+    x += z
+    x %= 26
+    z //= 1
+    x += 11
+    x = int(x == w)
+    x = int(x == 0)
+    y *= 0
+    y += 25
+    y *= x
+    y += 1
+    z *= y
+    y *= 0
+    y += w
+    y += 14
+    y *= x
+    z += y
+    w = num.pop(0)
+    x *= 0
+    x += z
+    x %= 26
+    z //= 26
+    x += -13
+    x = int(x == w)
+    x = int(x == 0)
+    y *= 0
+    y += 25
+    y *= x
+    y += 1
+    z *= y
+    y *= 0
+    y += w
+    y += 3
+    y *= x
+    z += y
+    w = num.pop(0)
+    x *= 0
+    x += z
+    x %= 26
+    z //= 1
+    x += 14
+    x = int(x == w)
+    x = int(x == 0)
+    y *= 0
+    y += 25
+    y *= x
+    y += 1
+    z *= y
+    y *= 0
+    y += w
+    y += 11
+    y *= x
+    z += y
+    w = num.pop(0)
+    x *= 0
+    x += z
+    x %= 26
+    z //= 1
+    x += 15
+    x = int(x == w)
+    x = int(x == 0)
+    y *= 0
+    y += 25
+    y *= x
+    y += 1
+    z *= y
+    y *= 0
+    y += w
+    y += 13
+    y *= x
+    z += y
+    w = num.pop(0)
+    x *= 0
+    x += z
+    x %= 26
+    z //= 26
+    x += -7
+    x = int(x == w)
+    x = int(x == 0)
+    y *= 0
+    y += 25
+    y *= x
+    y += 1
+    z *= y
+    y *= 0
+    y += w
+    y += 11
+    y *= x
+    z += y
+    w = num.pop(0)
+    x *= 0
+    x += z
+    x %= 26
+    z //= 1
+    x += 10
+    x = int(x == w)
+    x = int(x == 0)
+    y *= 0
+    y += 25
+    y *= x
+    y += 1
+    z *= y
+    y *= 0
+    y += w
+    y += 7
+    y *= x
+    z += y
+    w = num.pop(0)
+    x *= 0
+    x += z
+    x %= 26
+    z //= 26
+    x += -12
+    x = int(x == w)
+    x = int(x == 0)
+    y *= 0
+    y += 25
+    y *= x
+    y += 1
+    z *= y
+    y *= 0
+    y += w
+    y += 12
+    y *= x
+    z += y
+    w = num.pop(0)
+    x *= 0
+    x += z
+    x %= 26
+    z //= 1
+    x += 15
+    x = int(x == w)
+    x = int(x == 0)
+    y *= 0
+    y += 25
+    y *= x
+    y += 1
+    z *= y
+    y *= 0
+    y += w
+    y += 15
+    y *= x
+    z += y
+    w = num.pop(0)
+    x *= 0
+    x += z
+    x %= 26
+    z //= 26
+    x += -16
+    x = int(x == w)
+    x = int(x == 0)
+    y *= 0
+    y += 25
+    y *= x
+    y += 1
+    z *= y
+    y *= 0
+    y += w
+    y += 13
+    y *= x
+    z += y
+    w = num.pop(0)
+    x *= 0
+    x += z
+    x %= 26
+    z //= 26
+    x += -9
+    x = int(x == w)
+    x = int(x == 0)
+    y *= 0
+    y += 25
+    y *= x
+    y += 1
+    z *= y
+    y *= 0
+    y += w
+    y += 1
+    y *= x
+    z += y
+    w = num.pop(0)
+    x *= 0
+    x += z
+    x %= 26
+    z //= 26
+    x += -8
+    x = int(x == w)
+    x = int(x == 0)
+    y *= 0
+    y += 25
+    y *= x
+    y += 1
+    z *= y
+    y *= 0
+    y += w
+    y += 15
+    y *= x
+    z += y
+    w = num.pop(0)
+    x *= 0
+    x += z
+    x %= 26
+    z //= 26
+    x += -8
+    x = int(x == w)
+    x = int(x == 0)
+    y *= 0
+    y += 25
+    y *= x
+    y += 1
+    z *= y
+    y *= 0
+    y += w
+    y += 4
+    y *= x
+    z += y
+
+    return z
 
 def part1(instructions):
     for num in range(10**14, 0, -1):
